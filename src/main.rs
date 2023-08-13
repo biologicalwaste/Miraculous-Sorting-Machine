2260b9d0824be5b346e36de37455419478479b2e
biologicalwaste <samewas@protonmail.com>
2023-08-13 10:17:55 -0400

    testing things

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::array;
 fn main() {
-    println!("Hello, world!");
+    // Init the array and generate a random sequence to fill it
+    let rnd_array = generate_rndarray();
+
+    //Testing; print the whole array.
+    for i in 0..63 {
+        let current_display = rnd_array[i];
+        println!("{current_display}")
+    }
+}
+
+fn generate_rndarray() -> [i8; 64] {
+    let mut a: [i8; 64] = [0; 64];
+    for i in 0..63 {
+        // let mut a: [i8; 64] = [0; 64];
+    }
+
+    return a;
 }
